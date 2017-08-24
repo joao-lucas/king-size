0eb632d9d06f990cae55cb0f9f4d82dbe5fbe647
João Lucas <joaolucas@linuxmail.org>
2017-08-24 17:22:51 -0300

    iniciação a estrutura da função: monitorar uma ou todas as redes

--- kingsize.sh
+++ kingsize.sh
@@ -15,22 +15,39 @@
 #IPEXTERNO=`curl -s ipinfo.io/ip`
 #MACATUAL=`ip address | awk '/ether/ {print $2}'`
 #MACFALSO=
-#DIR=`pwd`
+DIR=`pwd`
 #ARQTMP=
-#ARQ=
+ARQ=teste
 
 
 
 #MENU
 
 # funçoes a serem criadas
-# change mac
-#set_interface
-#install_depends
-#check_depends
 #public_ip
 #verificar_usuario
 
+function verificar_dependencias(){
+	if ! hash yad 2> /dev/null; then
+		echo "[ FALHA ] yad dialog nao instalado!"
+		exit 1
+	fi
+
+	if ! hash aircrack-ng 2>/dev/null; then
+		echo "[ FALHA ] aircrack-ng nao instalado!"
+		exit 1
+	fi
+	
+	if ! hash xfce4-terminal 2> /dev/null; then
+		echo "[ FALHA ] xfce4-terminal nao instalado!"
+		exit 1	
+	fi
+
+	if ! hash reaver 2> /dev/null; then
+		echo "[ FALHA ] reaver nao instalado!"
+		exit 1
+	fi	
+}
 
 function iniciar_mon() {
 # Adiciona a interface de monitoramento wlan0mon. Caso contrario, emite mensagem de erro
@@ -45,35 +62,65 @@ function matar_processos_que_atrapalham_suite(){
 # Para verificar se existem processos que atrapalham a suite aircrack:
 # 	airmon-ng check
 
-# Matar processos
-airmon-ng check kill
+# Matar processos que atrapalham a suite aircrack-ng
+airmon-ng check kill &> /dev/null
 
 }
 
 function conf_interface(){
-ifconfig wlan0 up &> /dev/null
+ifconfig wlan0 &> /dev/null
+if [ $? != 0 ]; then
+	ifconfig wlan0 up &> /dev/null
+fi
 
 }
 
 function escanear_todas_redes(){
 # Escanear todas redes encontradas pelo adaptador de rede sem fio. Caso contrario, emite um erro.
-(xterm -title "Escaneando todas as redes" -e "airodump-ng wlan0mon" &) || \
-	echo -e "[ FALHA ] Ocorreram erros!"
+(xterm -geometry 85x25 -title "Escaneando todas as redes" -e "airodump-ng wlan0mon" &) || \
+echo -e "[ FALHA ] Ocorreram erros!"
 
 }
 
-function escanear_uma_rede() {
-if [ $# -lt 4 ]; then
-(xterm -title "Escaneando rede especifica" -e "airodump-ng \
-	--bssid $BSSID \
-	--essid $ESSID \
-	--channel $CHANNEL \
-	--write $OUTPUT/$ARQ $INTERFACE_MON" &) || \
-	echo -e "[ FALHA ] Ocorreram erros"
 
-else
-	echo "[ FALHA ] Falta de parametros para o escaneamento da rede!"
+function setar_parametros(){
+	PARAMETROS=$(yad --title "$TITLE" \
+	--form \
+	--field "BSSID" "" \
+	--field "ESSID" "" \
+	--field "Channel" "" \
+	--field "Interface Mon" "wlan0mon" \
+	#--field "Salvar em" "$DATE.cap" \
+	#--field "[ Wordlist ]":BTN "yad --file --maximized" \
+	#--field "[ Salvar na pasta ]":BTN "yad --title $TITLE --maximized --file --directory" \
+	--button ok \
+	--button cancel \
+	--undecorated \
+	--center & )
+
+	BSSID=$(echo "$PARAMETROS" | cut -d '|' -f 1)
+	ESSID=$(echo "$PARAMETROS" | cut -d '|' -f 2)
+	CHANNEL=$(echo "$PARAMETROS" | cut -d '|' -f 3)
+	INTERFACE_MON=$(echo "$PARAMETROS" | cut -d '|' -f 4)
+	#DIR=$(echo "$PARAMETROS" | cut -d '|' -f 5)
+	#ARQ=$(echo "$PARAMETROS" | cut -d '|' -f 5)
+
+	escanear_uma_rede
+}
+
+function escanear_uma_rede() {
+# Verifica se os 4 parametros obrigatorios para uso da função estão setados.
+if [ -z $BSSID  ] || [ -z $ESSID ] || [ -z $CHANNEL ] || [ -z INTERFACE_MON ] ; then
+	echo "[ FALHA ] Falta parametros para o escaneamento da rede!"
+	exit 1
 fi
+
+(xterm -geometry 85x25 -title "Escaneando rede especifica" -e "airodump-ng \
+--bssid $BSSID \
+--essid $ESSID \
+--channel $CHANNEL \
+--write $DIR/$ARQ $INTERFACE_MON" &) || \
+echo -e "[ FALHA ] Ocorreram erros"
 
 }
 
@@ -104,9 +151,11 @@ echo "[ FALHA ] Ocorreram erros em fazer o deauth do cliente: $CLIENTE"
 #}
 
 function matar_todos_processos() {
-killall aireplay-ng &> /dev/null
-killall dhcpd &> /dev/null
+
+iw dev wlan0mon del
 killall xterm &> /dev/null
+killall airodump-ng 2> /dev/null
+killall aireplay-ng &> /dev/null
 
 }
 
@@ -141,9 +190,10 @@ return 0
 #echo
 #}
 
-
+verificar_dependencias
 matar_processos_que_atrapalham_suite
 conf_interface
 iniciar_mon
 escanear_todas_redes
-matar_todos_processos
+setar_parametros
+#matar_todos_processos
